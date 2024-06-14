2e3777e0fa78a5014f74117044457924d6494574
Eliot Valdes <eliottvaldes@hotmail.com>
2024-06-14 07:32:39 -0600

    feat: Add route to get data from cities
    The code changes add a new route `/data` in the `cities_routes.py` file. This route handles a GET request and retrieves data from the cities. The data includes the id, estado, ciudad, nombre_recinto, latitud, and longitud. The results are returned as a JSON response.

--- routes/tsp/cities_routes.py
+++ routes/tsp/cities_routes.py
@@ -1,10 +1,36 @@
 from flask import Blueprint, request, jsonify
 tsp_cities = Blueprint('tsp_cities', __name__)
 
+# import helper function to get data from cities
+from helpers.get_data_from_cities import get_data_from_cities
 # import middleware
 from middlewares.tsp.cities_middleware import validate_tsp_request
 # import controller function
 from controllers.tsp_controller import run_tsp_algorithm
+
+"""
+@route: /data
+@method: GET
+@description: Get the data from the cities. Data includes the id,estado,ciudad,nombre_recinto,latitud,longitud
+@response: JSON
+"""
+@tsp_cities.route('/data', methods=['GET'])
+def get_data_tsp_cities():
+    try:
+        data = get_data_from_cities()        
+        if len(data) == 0:
+            raise Exception('No results found')        
+
+        return jsonify({
+            'ok': True,
+            'results': data
+            }), 200
+        
+    except Exception as e:
+        return jsonify({
+            'ok': False,
+            'error': str(e)
+        }), 500
 
 @tsp_cities.route('/solve', methods=['POST'])
 @validate_tsp_request
